1434753c91d25310c7d48d999d48209d4b4883d5
jainefer <jy5849@naver.com>
2025-05-26 08:24:20 +0900

    [fix] 뉴스 Mock 엔티티 패키지화 & 하드코딩 상수화

---
+++ src/test/java/com/youtil/Constants/NewsServiceConstants.java
@@ -0,0 +1,26 @@
+package com.youtil.Constants;
+
+public class NewsServiceConstants {
+
+    public static final String BASE_URL = "https://mock.domain.com";
+    public static final String MOCK_NEWS_URL = "http://example.com/news";
+    public static final String MOCK_PUB_DATE = "2024-04-01 10:00:00";
+    public static final String ORIGINAL_TITLE = "Original title";
+    public static final String TRANSLATED_TITLE = "번역된 제목";
+    public static final String DEFAULT_DESCRIPTION = "요약 내용";
+
+    public static final String PATH_RESULTS = "results";
+    public static final String PATH_DUPLICATE = "duplicate";
+    public static final String PATH_LINK = "link";
+    public static final String PATH_PUB_DATE = "pubDate";
+    public static final String PATH_TITLE = "title";
+    public static final String PATH_DESCRIPTION = "description";
+    public static final String PATH_IMAGE_URL = "image_url";
+    public static final String FALLBACK_DESCRIPTION = "요약본 미제공";
+    public static final String DEFAULT_IMAGE_URL = "https://example.com/image.jpg";
+    public static final String TARGET_LANG = "ko";
+
+    private NewsServiceConstants() {
+    }
+
+}
