3490027e26212627123774707e0cc53fab972680
jainefer <jy5849@naver.com>
2025-05-30 14:08:10 +0900

    [feat] TIL 생성 동시성 제어관련 상수화

--- src/main/java/com/youtil/Common/Constants/TilServiceConstants.java
+++ src/main/java/com/youtil/Common/Constants/TilServiceConstants.java
@@ -26,6 +26,8 @@ public class TilServiceConstants {
     public static final int MAX_TIL_WORKER_THREADS = 10;
     public static final int MAX_STREAM_FETCH_COUNT = 5;
 
+    public static final int RESEND_TIMEOUT_SECONDS = 360;
+
     private TilServiceConstants() {
     }
 }
