86502f2af17afaa17683fe500e5c3a3113b9e3ff
jainefer <jy5849@naver.com>
2025-06-23 14:55:29 +0900

    [fix] 예외처리 추가

--- src/main/java/com/youtil/Api/Interview/Handler/InterviewRequestHandler.java
+++ src/main/java/com/youtil/Api/Interview/Handler/InterviewRequestHandler.java
@@ -11,10 +11,12 @@ import com.youtil.Common.Constants.AiServiceConstants;
 import com.youtil.Common.Enums.AiType;
 import com.youtil.Common.Handler.AbstractAiRequestHandler;
 import com.youtil.Concurrency.RedisSemaphoreManager;
+import io.jsonwebtoken.io.SerializationException;
 import java.util.concurrent.PriorityBlockingQueue;
 import java.util.concurrent.ScheduledExecutorService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.data.redis.RedisConnectionFailureException;
 import org.springframework.data.redis.connection.stream.MapRecord;
 import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.stereotype.Component;
@@ -73,7 +75,16 @@ public class InterviewRequestHandler extends
                     objectMapper.writeValueAsString(errorResponse),
                     constants.getResultTtl());
         } catch (JsonProcessingException e) {
-            log.error("면접 에러 응답 저장 실패", e);
+            log.error("에러 응답 저장 실패", e);
+        } catch (RedisConnectionFailureException e) {
+            log.error("레디스 접속 에러", e);
+        } catch (SerializationException e) {
+            log.error("직력화 실패", e);
+
+        } catch (IllegalArgumentException e) {
+            log.error("부적절한 값 포함", e);
+        } catch (Exception e) {
+            log.error("알수없는 예외가 발생했습니다.", e);
         }
 
     }
--- src/main/java/com/youtil/Api/Tils/Handler/TilRequestHandler.java
+++ src/main/java/com/youtil/Api/Tils/Handler/TilRequestHandler.java
@@ -18,10 +18,12 @@ import com.youtil.Common.Constants.AiServiceConstants;
 import com.youtil.Common.Enums.AiType;
 import com.youtil.Common.Handler.AbstractAiRequestHandler;
 import com.youtil.Concurrency.RedisSemaphoreManager;
+import io.jsonwebtoken.io.SerializationException;
 import java.util.concurrent.PriorityBlockingQueue;
 import java.util.concurrent.ScheduledExecutorService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.data.redis.RedisConnectionFailureException;
 import org.springframework.data.redis.connection.stream.MapRecord;
 import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.stereotype.Component;
@@ -100,6 +102,15 @@ public class TilRequestHandler extends
                     constants.getResultTtl());
         } catch (JsonProcessingException e) {
             log.error("에러 응답 저장 실패", e);
+        } catch (RedisConnectionFailureException e) {
+            log.error("레디스 접속 에러", e);
+        } catch (SerializationException e) {
+            log.error("직력화 실패", e);
+
+        } catch (IllegalArgumentException e) {
+            log.error("부적절한 값 포함", e);
+        } catch (Exception e) {
+            log.error("알수없는 예외가 발생했습니다.", e);
         }
     }
 
