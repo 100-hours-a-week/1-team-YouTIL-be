05aba4395c90246301e538dff505829fc88830d7
kingwhangzang <soyun011112@sookmyung.ac.kr>
2025-07-02 15:55:34 +0900

    [fix] 상대 경로로 수정

--- src/main/java/com/youtil/Api/Github/Constants/GitHubApiConstants.java
+++ src/main/java/com/youtil/Api/Github/Constants/GitHubApiConstants.java
@@ -7,15 +7,15 @@ public final class GitHubApiConstants {
     public static final String BASE_URL = "https://api.github.com";
 
     // User 관련 URL
-    public static final String USER_INFO_URL = BASE_URL + "/user";
-    public static final String USER_ORGS_URL = BASE_URL + "/user/orgs";
-    public static final String USER_REPOS_URL = BASE_URL + "/user/repos";
-    public static final String USER_TEAMS_URL = BASE_URL + "/user/teams";
+    public static final String USER_INFO_URL = "/user";
+    public static final String USER_ORGS_URL = "/user/orgs";
+    public static final String USER_REPOS_URL = "/user/repos";
+    public static final String USER_TEAMS_URL = "/user/teams";
 
     // Repository 관련 URL
-    public static final String REPOSITORIES_BASE_URL = BASE_URL + "/repositories/";
-    public static final String REPOS_BASE_URL = BASE_URL + "/repos/";
-    public static final String TEAMS_BASE_URL = BASE_URL + "/teams/";
+    public static final String REPOSITORIES_BASE_URL = "/repositories/";
+    public static final String REPOS_BASE_URL = "/repos/";
+    public static final String TEAMS_BASE_URL = "/teams/";
 
     // URL 서브 경로
     public static final String BRANCHES_PATH = "/branches";
