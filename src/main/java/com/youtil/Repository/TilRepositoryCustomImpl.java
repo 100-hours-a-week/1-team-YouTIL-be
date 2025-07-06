20f90a4e3e125a571317ce163b61a9c2c6f2d736
kingwhangzang <soyun011112@sookmyung.ac.kr>
2025-07-06 20:14:33 +0900

    [fix] visitedCount, recommendCount 추가

--- src/main/java/com/youtil/Repository/TilRepositoryCustomImpl.java
+++ src/main/java/com/youtil/Repository/TilRepositoryCustomImpl.java
@@ -163,7 +163,9 @@ public class TilRepositoryCustomImpl implements TilRepositoryCustom {
                         til.id,
                         til.title,
                         til.tag,
-                        til.createdAt
+                        til.createdAt,
+                        til.visitedCount,
+                        til.recommendCount
                 ))
                 .from(til)
                 .join(til.user, user)
