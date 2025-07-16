4f008e3cc0307ea151fc81ad4dd7da9e2356bf5e
jainefer <jy5849@naver.com>
2025-07-16 14:42:54 +0900

    [fix] 꼬인거 해결

--- src/test/java/com/youtil/Mock/MockTilBuilder.java
+++ src/test/java/com/youtil/Mock/MockTilBuilder.java
@@ -13,7 +13,7 @@ import static com.youtil.Constants.MockTilConstants.MOCK_TITLE;
 import com.youtil.Model.Til;
 import com.youtil.Model.User;
 
-public class MockTilBuilder {
+public classMockTilBuilder {
 
     public static Til createMockTil(User user) {
         return Til.builder()
