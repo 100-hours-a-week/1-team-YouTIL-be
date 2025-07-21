01e3713d394939b00519c6d568b10a881e087973
kingwhangzang <soyun011112@sookmyung.ac.kr>
2025-07-21 15:23:34 +0900

    [feat] 중복 요청 방지 어노테이션 추가

---
+++ src/main/java/com/youtil/Common/DuplicatePrevention/PreventDuplicate.java
@@ -0,0 +1,24 @@
+package com.youtil.Common.DuplicatePrevention;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Target(ElementType.METHOD)
+@Retention(RetentionPolicy.RUNTIME)
+public @interface PreventDuplicate {
+
+    String action();
+
+    /**
+     * 제한 시간 (초)
+     */
+    int limitSeconds() default 5;
+
+    /**
+     * 중복 체크에 사용할 데이터 필드들
+     * Path Variable이나 Query Parameter 이름
+     */
+    String[] dataFields() default {};
+}
